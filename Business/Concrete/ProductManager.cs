3c68ea1dea111af4581c096389acb343fd2e8f9d
gorkemetem <79565642+gorkemetem@users.noreply.github.com>
2021-12-23 00:35:52 +0300

    son

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 
 
-//deneme yorumu-ekici-görkem-son
+//deneme yorumu-ekici-görkem-son-civan
 namespace Business.Concrete {
     public class ProductManager : IProductService {
 
